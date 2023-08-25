579edb90f44109dc214253dbfaf2cd981eb13791
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-25 15:12:28 +0300

    setting up

---
+++ script.js
@@ -0,0 +1,6 @@
+const firstNameInput = document.querySelector("#first-name-input");
+const lastNameInput = document.querySelector("#last-name-input");
+const emailInput = document.querySelector("#email-input");
+const clickToEditButton = document.querySelector("#click-to-edit");
+const submitButton = document.querySelector("#submit");
+const cancelButton = document.querySelector("#cancel");
