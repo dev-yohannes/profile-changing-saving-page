93f13aa420bc64f9570bed3477a65bf0d7ad1447
Yohannes Mulu <dev.yohannes@gmail.com>
2023-08-25 18:05:14 +0300

    adding span, adding remove classes and creating the logic. it doesn't include the click to edit button

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
 const firstNameInput = document.querySelector("#first-name-input");
+const firstNameSpan = document.querySelector("#first-name-span");
 const lastNameInput = document.querySelector("#last-name-input");
+const lastNameSpan = document.querySelector("#last-name-span");
 const emailInput = document.querySelector("#email-input");
+const emailSpan = document.querySelector("#email-span");
 const clickToEditButton = document.querySelector("#click-to-edit");
 const submitButton = document.querySelector("#submit");
 const cancelButton = document.querySelector("#cancel");
+
+clickToEditButton.classList.add("remove-button");
+firstNameSpan.classList.add("remove-span");
+lastNameSpan.classList.add("remove-span");
+emailSpan.classList.add("remove-span");
+
+function savingUserInfo() {
+  localStorage.setItem("firstName", firstNameInput.value);
+  localStorage.setItem("lastName", lastNameInput.value);
+  localStorage.setItem("email", emailInput.value);
+}
+
+submitButton.addEventListener("click", function () {
+  if (
+    firstNameInput.value === "" &&
+    lastNameInput.value === "" &&
+    emailInput.value === ""
+  ) {
+    alert("You need to fill the fields");
+    firstNameInput.classList.add("red-border-input");
+    lastNameInput.classList.add("red-border-input");
+    emailInput.classList.add("red-border-input");
+  } else if (firstNameInput.value === "") {
+    firstNameInput.classList.add("red-border-input");
+    alert("You need to fill First Name field");
+  } else if (lastNameInput.value === "") {
+    lastNameInput.classList.add("red-border-input");
+    alert("You need to fill Last Name field");
+  } else if (emailInput.value === "") {
+    emailInput.classList.add("red-border-input");
+    alert("You need to fill Email field");
+  } else {
+    // firstNameInput.classList.remove("red-border-input");
+    // lastNameInput.classList.remove("red-border-input");
+    // emailInput.classList.remove("red-border-input");
+
+    firstNameInput.classList.add("remove-inputs");
+    firstNameSpan.classList.remove("remove-span");
+    firstNameSpan.innerHTML = localStorage.getItem("firstName");
+
+    lastNameInput.classList.add("remove-inputs");
+    lastNameSpan.classList.remove("remove-span");
+    lastNameSpan.innerHTML = localStorage.getItem("lastName");
+
+    emailInput.classList.add("remove-inputs");
+    emailSpan.classList.remove("remove-span");
+    emailSpan.innerHTML = localStorage.getItem("email");
+
+    clickToEditButton.classList.remove("remove-button");
+    submitButton.classList.add("remove-button");
+    cancelButton.classList.add("remove-button");
+  }
+});
+
+cancelButton.addEventListener("click", function () {
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  emailInput.value = "";
+  firstNameInput.classList.remove("red-border-input");
+  lastNameInput.classList.remove("red-border-input");
+  emailInput.classList.remove("red-border-input");
+});
